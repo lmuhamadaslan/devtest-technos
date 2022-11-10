b43a93773145b3251ae55793b52c499c41cc6806
lmuhamadaslan <lmuhamadaslan@gmail.com>
2022-11-10 22:58:58 +0800

    initial commit

--- application/models/ProdukModel.php
+++ application/models/ProdukModel.php
@@ -9,8 +9,8 @@ class ProdukModel extends CI_Model
 
     public function getProduk()
     {
-        // TO DO CODE
-        $query = $this->db->query("SELECT kategori, COUNT(*) AS Jumlah FROM tbl_data GROUP BY kategori");
+        // TO DO CODE    
+        $query = $this->db->query("SELECT tbl_kategori.id, tbl_kategori.category, COUNT(tbl_data.id) AS Jumlah FROM tbl_data INNER JOIN tbl_kategori ON tbl_data.category_id = tbl_kategori.id GROUP BY tbl_kategori.id;");
         return $query->result_array();
     }
 }
